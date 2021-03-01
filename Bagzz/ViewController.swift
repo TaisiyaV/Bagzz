b197a2c447716a71c5a684f072c082cce2a368c7
TasyaV <tasya-vas@mail.ru>
2021-03-01 10:36:46 +0300

    Initial Commit

---
+++ Bagzz/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Bagzz
+//
+//  Created by tasya on 01.03.2021.
+//  Copyright © 2021 Taisiya V. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
